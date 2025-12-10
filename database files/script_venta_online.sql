0474559c6c82a65c9975d01b3c830f4ad8b54da4
Jaipel <javierdelmoral04@gmail.com>
2025-12-09 22:34:28 -0400

    feat: venta_online script

--- database files/script_venta_online.sql
+++ database files/script_venta_online.sql
@@ -5,6 +5,7 @@ SET LINESIZE 200;
 
 UNDEFINE v_id_cliente;
 UNDEFINE v_lista_productos;
+UNDEFINE v_id_pais_residencia;
 
 PROMPT
 PROMPT ========================================================================
@@ -27,25 +28,45 @@ JOIN v_saldo_puntos_cliente vsc ON c.id_lego = vsc.id_cliente
 ORDER BY c.id_lego;
 
 PROMPT
+PROMPT [ACCION REQUERIDA - ID DE CLIENTE]
+ACCEPT v_id_cliente NUMBER PROMPT '>>> Por favor, escriba el ID del Cliente: '
+
+COLUMN ID_PAIS_TEMP NEW_VALUE v_id_pais_residencia NOPRINT
+SELECT c.id_pais_res ID_PAIS_TEMP FROM Clientes c WHERE c.id_lego = &v_id_cliente;
+COLUMN ID_PAIS_TEMP CLEAR
+
+PROMPT
 PROMPT ========================================================================
 PROMPT                       CATALOGO DE PRODUCTOS
 PROMPT ========================================================================
-COLUMN ID FORMAT 9999
-COLUMN NOMBRE FORMAT A30
-COLUMN PRECIO FORMAT $999.99
+COLUMN ID_JUGUETE FORMAT 9999
+COLUMN NOMBRE_JUGUETE FORMAT A30
+COLUMN PRECIO_ACTUAL FORMAT $999.99
+COLUMN ID_PAIS FORMAT 999
+COLUMN PAIS_DISPONIBLE FORMAT A20
+COLUMN LIMITE_POR_PAIS FORMAT 99
 
 -- Muestra solo los juguetes con precio activo
-SELECT j.id_juguete ID,
-       j.nombre NOMBRE,
-       hp.precio PRECIO_ACTUAL
-FROM Juguetes j
-JOIN Historico_Precios hp ON j.id_juguete = hp.id_juguete
-WHERE hp.fecha_fin IS NULL
-ORDER BY j.id_juguete;
-
-PROMPT
-PROMPT [ACCION REQUERIDA - ID DE CLIENTE]
-ACCEPT v_id_cliente NUMBER PROMPT '>>> Por favor, escriba el ID del Cliente: '
+SELECT
+    j.id ID_JUGUETE,
+    j.nombre NOMBRE_JUGUETE,
+    hp.precio PRECIO_ACTUAL,
+    p.id ID_PAIS,
+    p.nombre PAIS_DISPONIBLE,
+    cp.limite LIMITE_POR_PAIS
+FROM
+    Juguetes j
+JOIN
+    Historico_Precios hp ON j.id = hp.id_juguete
+JOIN
+    Catalogo_Paises cp ON j.id = cp.id_juguete
+JOIN
+    Paises p ON cp.id_pais = p.id
+WHERE
+    hp.fecha_fin IS NULL
+    AND cp.id_pais = &v_id_pais_residencia -- <<< FILTRO APLICADO
+ORDER BY
+    j.id, p.id;
 
 PROMPT
 PROMPT [ACCION REQUERIDA - LISTA DE PRODUCTOS]
